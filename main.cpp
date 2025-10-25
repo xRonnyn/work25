c6d7874867fa29d449853c9006202ea81e2011de
xRonnyn <pashuha.k@gmail.com>
2025-10-25 14:36:41 +0300

    1 version of main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 int main(){
   using u_t = unsigned;
   u_t a = 0;
-  std::cin >> a;
+  size_t count = 0;
+  //std::cin >> a;
+  while (std::cin >> a){
+    count ++;
+  }
   if (std::cin.eof()){
-    std::cout << 1;
+    std::cout << count;
     std::cout << "\n";
   }
   else if(std::cin.fail()){
