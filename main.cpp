3a253b748988dcedbafcc4d86927f74b17214808
xRonnyn <pashuha.k@gmail.com>
2025-10-25 14:25:43 +0300

    base case main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
+#include <iostream>
 int main(){
   using u_t = unsigned;
   u_t a = 0;
   std::cin >> a;
+  if (std::cin.eof()){
+    std::cout << 1;
+    std::cout << "\n";
+  }
+  else if(std::cin.fail()){
+    std::cerr << "Input err\n";
+    return 1;
+  }
 }
