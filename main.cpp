566bc5eb921d20172d24e4aeca11b015601b0f31
xRonnyn <pashuha.k@gmail.com>
2025-10-25 15:02:07 +0300

    isPyth stable

--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@ bool isPyth(unsigned a, unsigned b, unsigned c);
 int main(){
   using u_t = unsigned;
   u_t a = 0, b = 0, c =0;
+  std::cin >> b >> c;
   size_t count = 0;
-  //std::cin >> a;
   while (std::cin >> a){
     count += isPyth(a,b,c) ? 1 : 0;
+    c = b; 
+    b = a;
   }
   if (std::cin.eof()){
     std::cout << count;
@@ -18,8 +20,8 @@ int main(){
   }
 }
 bool isPyth(unsigned a, unsigned b, unsigned c){
-  bool p = a*a = b*b + c*c;
-  p = p || b*b = a*a + c*c;
-  p = p || c*c = a*a+b*b;
+  bool p = a*a == (b*b + c*c);
+  p = p || (b*b == (a*a + c*c));
+  p = p || (c*c == (a*a + b*b));
   return p;
 }
